38a999f6e36e20daa836f9535f87fb733ad868b3
Caleb <calebley@hotmail.com>
2022-04-02 15:31:42 +0800

    done

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,25 +1,61 @@
 import urlcat from "urlcat"
-import { useEffect } from "react"
+import { useState } from "react"
 const BACKEND = process.env.REACT_APP_BACKEND
 const url = urlcat(BACKEND, "/api/holidays")
 
 
 
-    function Create() {
-        useEffect(() => {
-            fetch(url, { method: "POST", headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({name: "myBirthday"})
+function Create() {
+    const [name, setName] = useState("");
+    const [likes, setLikes] = useState(0);
+    const [error, setError] = useState("")
+    
+  
+    const createHoliday = (holiday) => {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(holiday),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.error) {
+                setError(data.error)
+            }
         })
-                .then((response) => response.json())
-                .then((data) => console.log(data))
-        }, [])
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log( { name, likes})
+    };
+  
     return (
-        <>
-        Create
-        </>
-    )
-}
+      <>
+        <form onSubmit={handleSubmit}>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <br />
+          Likes:
+          <input
+            type="number"
+            name="likes"
+            value={likes}
+            onChange={(event) => setLikes(event.target.value)}
+          />
+          <p>{error}</p>
+          <br />
+          <button>Create</button>
+        </form>
+      </>
+    );
+  }
 
 export default Create
