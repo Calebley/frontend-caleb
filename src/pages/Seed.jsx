83960ae0cfeb30d5d64556324fa5efe4ec2c8fe5
Caleb <calebley@hotmail.com>
2022-04-02 12:04:58 +0800

    seeded

---
+++ src/pages/Seed.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+function Seed() {
+    const [seed, setSeed] = useState([])
+
+useEffect(() => {
+fetch("http://localhost:2000/api/holidays/seed")
+.then(response => response.json())
+.then(data => setSeed(data))
+}, [])
+
+return(
+        <>
+        <h1>Seed</h1>
+        <pre>
+            {JSON.stringify(seed, null, 2)} </pre>
+        </>
+    )
+}
+
+export default Seed
