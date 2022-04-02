1d9e6517906ffb124a833d0e502e635888a34968
Caleb <calebley@hotmail.com>
2022-04-02 12:12:43 +0800

    done

--- src/pages/Seed.jsx
+++ src/pages/Seed.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react"
 
+const BACKEND = process.env.REACT_APP_BACKEND ?? "http://localhost:2000"
+
 function Seed() {
     const [seed, setSeed] = useState([])
 
 useEffect(() => {
-fetch("http://localhost:2000/api/holidays/seed")
-.then(response => response.json())
-.then(data => setSeed(data))
-}, [])
+    fetch(`${BACKEND}/api/holidays/seed`)
+    .then(response => response.json())
+    .then(data => setSeed(data))
+    }, [])
 
 return(
         <>
-        <h1>Seed</h1>
+        <h1>Seed 2</h1>
         <pre>
             {JSON.stringify(seed, null, 2)} </pre>
         </>
